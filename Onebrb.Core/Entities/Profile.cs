437378ca7396db5ec4d057bb45c8567465ab14e9
kaldren <drenski666@gmail.com>
2019-12-22 15:56:02 +0200

    Profile entity

---
+++ Onebrb.Core/Entities/Profile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onebrb.Core.Entities
+{
+    class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
