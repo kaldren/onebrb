996d5869d31d6e76459d660e115ba8b3dc74513f
kaldren <drenski666@gmail.com>
2019-12-22 16:48:10 +0200

    IProfileRepository

---
+++ Onebrb.Core/Interfaces/IProfileRepository.cs
@@ -0,0 +1,24 @@
+﻿using Onebrb.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onebrb.Core.Interfaces
+{
+    interface IProfileRepository
+    {
+        /// <summary>
+        /// Get profile by Id
+        /// </summary>
+        /// <param name="id">Profile id</param>
+        /// <returns></returns>
+        Profile GetProfile(Guid id);
+
+        /// <summary>
+        /// Get profile by email
+        /// </summary>
+        /// <param name="email">Email address</param>
+        /// <returns></returns>
+        Profile GetProfile(string email);
+    }
+}
