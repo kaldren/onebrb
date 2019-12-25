47c4652203ab3f55d99adf41485371be1c79fe19
kaldren <drenski666@gmail.com>
2019-12-25 10:20:20 +0200

    Custom exception - EmailAddressNotFoundException

---
+++ Onebrb.Core/Exceptions/EmailAddressNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onebrb.Core.Exceptions
+{
+    public class EmailAddressNotFoundException : Exception
+    {
+        public EmailAddressNotFoundException() { }
+
+        public EmailAddressNotFoundException(string message) : base (message) { }
+
+        public EmailAddressNotFoundException(string message, Exception inner) : base(message, inner) { }
+    }
+}
