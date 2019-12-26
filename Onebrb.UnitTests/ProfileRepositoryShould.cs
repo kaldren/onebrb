7bcbc58be40f5a3fa2f98c803c427397c1fa585b
kaldren <drenski666@gmail.com>
2019-12-26 23:51:43 +0200

    Added Moq

---
+++ Onebrb.UnitTests/ProfileRepositoryShould.cs
@@ -0,0 +1,25 @@
+﻿using Onebrb.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Onebrb.UnitTests
+{
+    public class ProfileRepositoryShould
+    {
+        private readonly IProfileRepository _profileRepository;
+
+        public ProfileRepositoryShould()
+        {
+
+        }
+
+        [Fact]
+        public void ReturnProfileById()
+        {
+        }
+    }
+}
