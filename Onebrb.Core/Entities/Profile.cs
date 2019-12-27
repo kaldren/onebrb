3508dd7d4da87f1f59d168296f6adb6292ab4835
kaldren <drenski666@gmail.com>
2019-12-27 10:24:28 +0200

    Added profile type enum to Profile

--- Onebrb.Core/Entities/Profile.cs
+++ Onebrb.Core/Entities/Profile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Onebrb.Core.Enumerations;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,5 +11,6 @@ namespace Onebrb.Core.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public ProfileTypeEnum ProfileType { get; set; }
     }
 }
