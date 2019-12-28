8363d0c21db300b282e0c0e274fd292f513e4ca0
kaldren <drenski666@gmail.com>
2019-12-28 13:31:35 +0200

    Added Product component

---
+++ Onebrb.Spa/Pages/Product/ProductBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onebrb.Spa.Pages.Product
+{
+    public class ProductBase : ComponentBase
+    {
+        [Parameter]
+        public int ProductId { get; set; }
+    }
+}
