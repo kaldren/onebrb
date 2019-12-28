5076c862a937e34bb1f6f770dfd8bc3dedcd8263
kaldren <drenski666@gmail.com>
2019-12-28 16:14:51 +0200

    ProductService wip

---
+++ Onebrb.Spa/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Onebrb.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onebrb.Spa.Services
+{
+    public interface IProductService
+    {
+        /// <summary>
+        /// Creates product asynchronously
+        /// </summary>
+        /// <param name="product">The product</param>
+        /// <returns>The product</returns>
+        Task<Product> CreateProductAsync(Product product);
+    }
+}
---
+++ Onebrb.Spa/Services/ProductService.cs
@@ -0,0 +1,17 @@
+﻿using Onebrb.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onebrb.Spa.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<Product> CreateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
