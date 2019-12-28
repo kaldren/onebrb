7edc5458d9dc96e3f535ada37e38886f898dd5b4
kaldren <drenski666@gmail.com>
2019-12-28 16:12:10 +0200

    ProductRepository

---
+++ Onebrb.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Onebrb.Core.Entities;
+using Onebrb.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onebrb.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Product> CreateProductAsync(Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
